32eb82dd2e56d81b54eef6efc16fc8877c5e9205
cBombs <richie.ucsb@gmail.com>
2020-10-25 15:10:55 -0700

    xyz

--- Lab9/Python_Rocks.py
+++ Lab9/Python_Rocks.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 import RPi_I2C_driver, time
 
-mylcd = RPi_I2C_driver.lcd(addr = 0x27)
-mylcd.lcd_display_string("Python3 rocks...", 1)
+mylcd = RPi_I2C_driver.lcd()
+mylcd.lcd_display_string("Python3 rocks", 1)
 time.sleep(3)
 mylcd.lcd_clear()
 mylcd.backlight(0)
