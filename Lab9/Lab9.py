1aadf7950c6115671ebcb5d909b3d25566697c3e
cBombs <richie.ucsb@gmail.com>
2020-10-26 14:27:48 -0700

    dsgds

--- Lab9/Lab9.py
+++ Lab9/Lab9.py
@@ -2,8 +2,8 @@
 import RPi_I2C_driver, time
 
 mylcd = RPi_I2C_driver.lcd()
-mylcd.lcd_display_string("Time: ", 1)
-mylcd.lcd_display_string("xx", 1)
+mylcd.lcd_display_string("Time: " + "XX", 1)
+# str(datetime.datetime.now().time())
 mylcd.lcd_display_string("Temp: ", 2)
 time.sleep(3)
 mylcd.lcd_clear()
