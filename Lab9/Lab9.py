11f067fb69641cab263e0967a6ca8f18091a7d39
cBombs <richie.ucsb@gmail.com>
2020-10-26 14:35:57 -0700

    time x

--- Lab9/Lab9.py
+++ Lab9/Lab9.py
@@ -35,7 +35,7 @@ Fahrenheit = 9.0/5.0 * Celsius + 32
 
 mylcd = RPi_I2C_driver.lcd()
 mylcd.lcd_display_string("Time: " + str(datetime.datetime.now().time()), 1)
-mylcd.lcd_display_string("Temp: "+ str(Celsius), 2)
+mylcd.lcd_display_string("Temp: "+ str(Celsius) + " C", 2)
 time.sleep(3)
 mylcd.lcd_clear()
 mylcd.backlight(0)
